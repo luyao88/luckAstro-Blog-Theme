a9a6b9bfeeaf431cb7e33ea4b7822a4b6284e37d
superma <mayazhou2009@126.com>
2025-05-20 18:27:28 +0800

    feat(static files): 更新测试

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,19 @@
 export default {
   // 网站标题
-  Title: '韩小韩博客',
+  Title: 'S͛u͛p͛e͛r͛M͛a͛҇的博客',
   // 网站地址
-  Site: 'https://www.vvhan.com',
+  Site: 'https://luyao88.com',
   // 网站副标题
   Subtitle: '不曾与你分享的时间,我在进步.',
   // 网站描述
-  Description: '韩小韩博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
+  Description:
+    'Superma博客 专注于全栈开发与游戏引擎相关技术的实战学习分享，涵盖Java、H5、JS、Unity、Cocos等，并涉及Node、Python、Linux、等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
   // 网站作者
-  Author: '.𝙃𝙖𝙣',
+  Author: '.🆂🆄🅿🅴🆁🅼🅰',
   // 作者头像
   Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640',
   // 网站座右铭
-  Motto: '运气是计划之外的东西.',
+  Motto: '爱出者爱返 福往者福来',
   // Cover 网站缩略图
   Cover: '/assets/images/banner/072c12ec85d2d3b5.webp',
   // 网站侧边栏公告 (不填写即不开启)
@@ -32,21 +33,22 @@ export default {
     // 其他页面高度
     PageHeight: '28.88rem',
     // 背景
-    background: "url('/assets/images/home-banner.webp') no-repeat center 60%/cover",
+    background:
+      "url('/assets/images/home-banner.webp') no-repeat center 60%/cover",
   },
   // 博客主题配置
   Theme: {
     // 颜色请用 16 进制颜色码
     // 主题颜色
-    "--vh-main-color": "#01C4B6",
+    '--vh-main-color': '#01C4B6',
     // 字体颜色
-    "--vh-font-color": "#34495e",
+    '--vh-font-color': '#34495e',
     // 侧边栏宽度
-    "--vh-aside-width": "318px",
+    '--vh-aside-width': '318px',
     // 全局圆角
-    "--vh-main-radius": "0.88rem",
+    '--vh-main-radius': '0.88rem',
     // 主体内容宽度
-    "--vh-main-max-width": "1458px",
+    '--vh-main-max-width': '1458px',
   },
   // 导航栏 (新窗口打开 newWindow: true)
   Navs: [
@@ -58,17 +60,34 @@ export default {
     { text: '昔日', link: '/archives', icon: 'Nav_archives' },
     { text: '留言', link: '/message', icon: 'Nav_message' },
     { text: '关于', link: '/about', icon: 'Nav_about' },
-    { text: 'API', link: 'https://api.vvhan.com/', target: true, icon: 'Nav_link' },
+    {
+      text: 'API',
+      link: 'https://api.vvhan.com/',
+      target: true,
+      icon: 'Nav_link',
+    },
   ],
   // 侧边栏个人网站
   WebSites: [
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
-    { text: 'Github', link: 'https://github.com/uxiaohan', icon: 'WebSite_github' },
+    {
+      text: 'Github',
+      link: 'https://github.com/uxiaohan',
+      icon: 'WebSite_github',
+    },
     { text: '韩小韩API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
     { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
-    { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
-    { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
+    {
+      text: '骤雨重山图床',
+      link: 'https://wp-cdn.4ce.cn',
+      icon: 'WebSite_img',
+    },
+    {
+      text: 'HanAnalytics',
+      link: 'https://analytics.vvhan.com',
+      icon: 'WebSite_analytics',
+    },
   ],
   // 侧边栏展示
   AsideShow: {
@@ -79,7 +98,7 @@ export default {
     // 是否展示标签
     TagsShow: true,
     // 是否展示推荐文章
-    recommendArticleShow: true
+    recommendArticleShow: true,
   },
   // DNS预解析地址
   DNSOptimization: [
@@ -88,7 +107,7 @@ export default {
     'https://analytics.vvhan.com',
     'https://vh-api.4ce.cn',
     'https://registry.npmmirror.com',
-    'https://pagead2.googlesyndication.com'
+    'https://pagead2.googlesyndication.com',
   ],
   // 博客音乐组件解析接口
   vhMusicApi: 'https://vh-api.4ce.cn/blog/meting',
@@ -97,37 +116,41 @@ export default {
     // Twikoo 评论
     Twikoo: {
       enable: false,
-      envId: ''
+      envId: '',
     },
     // Waline 评论
     Waline: {
       enable: false,
-      serverURL: ''
-    }
+      serverURL: '',
+    },
   },
   // Han Analytics 统计（https://github.com/uxiaohan/HanAnalytics）
-  HanAnalytics: { enable: true, server: 'https://analytics.vvhan.com', siteId: 'Hello-HanHexoBlog' },
+  HanAnalytics: {
+    enable: true,
+    server: 'https://analytics.vvhan.com',
+    siteId: 'Hello-HanHexoBlog',
+  },
   // Google 广告
   GoogleAds: {
     ad_Client: '', //ca-pub-xxxxxx
     // 侧边栏广告(不填不开启)
     asideAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`,
     // 文章页广告(不填不开启)
-    articleAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`
+    articleAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`,
   },
   // 文章内赞赏码
   Reward: {
     // 支付宝收款码
     AliPay: '/assets/images/alipay.webp',
     // 微信收款码
-    WeChat: '/assets/images/wechat.webp'
+    WeChat: '/assets/images/wechat.webp',
   },
   // 访问网页 自动推送到搜索引擎
   SeoPush: {
     enable: false,
     serverApi: '',
-    paramsName: 'url'
+    paramsName: 'url',
   },
   // 页面阻尼滚动速度
-  ScrollSpeed: 666
+  ScrollSpeed: 666,
 }
